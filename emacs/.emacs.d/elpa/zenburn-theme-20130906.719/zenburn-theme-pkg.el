2dd690bd08b6731b4de147ae846a66aa2a9da31f
Kuba Kuźma <kuba@jah.pl>
2013-09-09 22:40:23 +0200

    initial import

---
+++ emacs/.emacs.d/elpa/zenburn-theme-20130906.719/zenburn-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zenburn-theme" "20130906.719" "A low contrast color theme for Emacs. [github]" (quote nil))
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,44 @@
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-by-copying t)
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.backups"))))
+ '(browse-url-browser-function (quote browse-url-chromium))
+ '(browse-url-generic-program nil)
+ '(coffee-tab-width 2)
+ '(erc-nick "qoobaa")
+ '(erc-prompt-for-password t)
+ '(global-linum-mode t)
+ '(ido-enable-flex-matching t)
+ '(ido-max-directory-size 100000)
+ '(ido-mode (quote both) nil (ido))
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message "")
+ '(js2-enter-indents-newline t)
+ '(js2-mode-escape-quotes nil)
+ '(menu-bar-mode nil)
+ '(package-archives (quote (("melpa" . "http://melpa.milkbox.net/packages/") ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(require-final-newline t)
+ '(scroll-bar-mode nil)
+ '(show-trailing-whitespace t)
+ '(tool-bar-mode nil)
+ '(user-mail-address "kuba@jah.pl"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Inconsolata")))))
+
+(load-theme 'zenburn t)
+
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+(add-hook 'ruby-mode-hook 'ruby-end-mode)
+(add-hook 'ruby-mode-hook 'ruby-tools-mode)
