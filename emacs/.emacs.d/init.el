91d94c29400958aa0779abaf910e377be71adae9
Kuba Kuźma <kuba@jah.pl>
2014-01-07 19:23:56 +0100

    added default js2-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -42,3 +42,4 @@
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 (add-hook 'ruby-mode-hook 'ruby-end-mode)
 (add-hook 'ruby-mode-hook 'ruby-tools-mode)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
