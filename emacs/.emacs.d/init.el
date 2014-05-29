b62ed66a4e27586382d5bfc0ae7e3eaacbb6aff9
Kuba Kuźma <kuba@jah.pl>
2014-05-29 22:49:21 +0200

    added Gemfile to alist

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -43,6 +43,8 @@
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 (add-hook 'ruby-mode-hook 'ruby-end-mode)
 (add-hook 'ruby-mode-hook 'ruby-tools-mode)
+
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
